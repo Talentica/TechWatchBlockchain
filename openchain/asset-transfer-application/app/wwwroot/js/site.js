2bda2515f7a84a58e974af7fc00df02b0d66de49
Mohammed Moiyadi <mmoiyadi@realization.com>
2018-02-06 15:54:32 +0530

    first commit

---
+++ openchain/asset-transfer-application/app/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// Write your JavaScript code.
+// account-register.js
+(function () {
+
+    "use strict";
+
+    var baseUrl = $('base').attr('href');
+    var appBaseUrl = '';//baseUrl;
+    //alert("baseUrl:" + baseUrl);
+
+    //Creating the module
+    angular.module("openchain", ["ngRoute"])
+        .config(function ($routeProvider, $locationProvider) {
+            // Following is to fix unnecessary ! after # in the route
+            $locationProvider.hashPrefix('');
+
+            //$routeProvider.when("/registerinit", {
+            //    controller: "registerinitController",
+            //    controllerAs: "vm",
+            //    templateUrl: appBaseUrl + "views/registerinit.html"
+
+            //});
+
+            $routeProvider.when("/", {
+                controller: "mainViewController",
+                controllerAs: "vm",
+                templateUrl:  "/views/mainView.html"
+
+            });
+            
+            $routeProvider.otherwise({ redirectTo: "/" });
+
+        });
+
+})();
